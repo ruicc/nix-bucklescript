7a6b83ce50622eed9e9c8fbe39d31f5808d651ad
hiroqn <hiroqn1008@gmail.com>
2020-03-25 17:10:40 +0900

    :sparkles: add niv

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
+let
+  source = import ./nix/sources.nix;
+  nixpkgs = import source.nixpkgs {};
+  inherit (import source.niv {}) niv;
+in
+{ nixpkgs ? nixpkgs }:
 {
     bucklescript_4 = import ./4/bucklescript.nix;
-    bucklescript_5 = import ./5/;
+    bucklescript_5 = import ./5/default.nix;
+    bucklescript_6 = import ./6/bucklescript.nix;
 }
