496c02dd45d9138f3a362a1a0f903c261dddec27
Ruichi Kousuke <kousuke.ruichi@herp.co.jp>
2021-01-05 15:54:33 +0900

    Replace with rescript-compiler
    BuckleScript repo seems archived or deleted.

--- 4/src.nix
+++ 4/src.nix
@@ -1,7 +1,7 @@
 { fetchFromGitHub }:
 fetchFromGitHub {
-  owner = "BuckleScript";
-  repo = "bucklescript";
-  rev = "4.0.18";
-  sha256 = "0ql5ivf0bjmb8495ak2lx9wis3yll64irn8rh7nbn4rrpfvr4vfb";
+  owner = "rescript-lang";
+  repo = "rescript-compiler";
+  rev = "8.4.2";
+  sha256 = "1kk3916phasic4magy2pwxgv3b876r0aq371xlkmpfbnh8avsv55";
 }
